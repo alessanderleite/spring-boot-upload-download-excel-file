eb7775adeb7257641053e7585711356386966172
Alessander <alessander.logistica@gmail.com>
2020-05-23 19:57:24 -0300

    initial commit

---
+++ src/main/java/com/example/app/SpringBootUploadDownloadExcelFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadDownloadExcelFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadDownloadExcelFileApplication.class, args);
+	}
+
+}
